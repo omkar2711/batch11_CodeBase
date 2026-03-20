701a51313b6bcc313dacb1b5f67df9922be5899f
omkar2711 <omkarbharitkar20@ece.iiitp.ac.in>
2026-03-20 17:12:20 +0530

    project + revision

---
+++ KeyWords.java
@@ -0,0 +1,22 @@
+ class Animal {
+
+    final int legs = 4; // This variable cannot be changed once initialized
+
+    public Animal() {
+        System.out.println("Animal constructor called");
+    }
+    
+}
+
+class Dog extends Animal {
+   Dog(){
+        super();
+        System.out.println("Dog constructor called");
+    }
+}
+
+public class KeyWords {
+    public static void main(String[] args) {
+       
+    }
+}
