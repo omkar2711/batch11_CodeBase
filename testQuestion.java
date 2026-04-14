ba4a8cf3bc7818a1b34ad00bb02a55420f2ac3be
omkar2711 <omkarbharitkar20@ece.iiitp.ac.in>
2026-04-14 16:19:16 +0530

    add StringImp and testQuestion classes with string manipulation and Fibonacci sequence logic

---
+++ testQuestion.java
@@ -0,0 +1,20 @@
+
+public class testQuestion {
+
+   
+
+
+    public static void main(String[] args) {
+       
+        int a = 0;
+        int b = 1;
+
+        for(int i = 0;i<=7;i++){
+            System.out.println(a);
+            int c = a + b;
+            a = b;
+            b = c;
+
+        }
+    }
+}
