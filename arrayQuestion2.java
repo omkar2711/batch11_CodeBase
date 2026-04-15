b58008278cb46310de0f083db67bcaaa02b91c98
omkar2711 <omkarbharitkar20@ece.iiitp.ac.in>
2026-04-15 16:22:32 +0530

    refactor: comment out existing code in multiple Java files for cleanup

--- arrayQuestion2.java
+++ arrayQuestion2.java
@@ -1,29 +1,29 @@
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class arrayQuestion2 {
-    public static void main(String[] args) {
+// public class arrayQuestion2 {
+//     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+//         Scanner sc = new Scanner(System.in);
 
-        int arr[][] = new int[3][3];
+//         int arr[][] = new int[3][3];
 
-        for(int i = 0;i<3;i++){
-            for(int j = 0;j<3;j++){
-                int ele = sc.nextInt();
-                arr[i][j] = ele;
-            }
-        }
+//         for(int i = 0;i<3;i++){
+//             for(int j = 0;j<3;j++){
+//                 int ele = sc.nextInt();
+//                 arr[i][j] = ele;
+//             }
+//         }
 
-        int sum = 0;
+//         int sum = 0;
 
-         for(int i = 0;i<3;i++){
-            for(int j = 0;j<3;j++){
-                sum += arr[i][j];
-            }
-            System.out.println();
-        }
+//          for(int i = 0;i<3;i++){
+//             for(int j = 0;j<3;j++){
+//                 sum += arr[i][j];
+//             }
+//             System.out.println();
+//         }
 
-        System.out.println("Sum of all elements in the array is: " + sum);
-    }
-}
+//         System.out.println("Sum of all elements in the array is: " + sum);
+//     }
+// }
